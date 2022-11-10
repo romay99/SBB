3e942ebd5ef0fb146b6204be0ccb4fd911481b6d
SooBin_Lee <i_romay@naver.com>
2022-11-10 23:18:19 +0900

    first commit

---
+++ src/main/java/hello/itemservice/MainContorller.java
@@ -0,0 +1,18 @@
+package hello.itemservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+public class MainContorller {
+
+    @RequestMapping("/sbb")
+    @ResponseBody
+    public String index(){
+        return "HI!";
+    }
+
+}
---
+++ src/main/java/hello/itemservice/Question.java
@@ -0,0 +1,31 @@
+package hello.itemservice;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalTime creatDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+
+}
